17bc304d98be6611b8661d1a1e09aeadd4456504
Nozomi2225 <159567987+GuitarHero2@users.noreply.github.com>
2024-08-22 12:40:25 +0200

    Main Menu Update
    What's new:
    
    -Added support for four languages (English, Japanese, Spanish and Swedish)
    
    -Added a main menu prototype.

--- Assets/Scripts/MainMenu/MainMenuButtonBehavior.cs
+++ Assets/Scripts/MainMenu/MainMenuButtonBehavior.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class MainMenuButtonBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void QuitProgram()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Application.Quit();
+        Debug.Log("The program closed successfully!");
     }
 }

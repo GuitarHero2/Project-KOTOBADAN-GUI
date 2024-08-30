6230ac8e29feb75a75e3e1a8ad2bbca94f7a1f7c
Kobayashi Nozomi <159567987+GuitarHero2@users.noreply.github.com>
2024-08-30 19:01:57 +0200

    Word Dict UPdate.
    - UPdated the interface of the word dict to be more readable.

--- Assets/Scripts/DictManagement/DictManager.cs
+++ Assets/Scripts/DictManagement/DictManager.cs
@@ -24,19 +24,43 @@ public class DictManager : MonoBehaviour
     public TMP_Text wordDef8;
     public TMP_Text wordDef9;
     public TMP_Text wordDef10;
+    public TMP_Text wordDef11;
+    public TMP_Text wordDef12;
+    public TMP_Text wordDef13;
+    public TMP_Text wordDef14;
+    public TMP_Text wordDef15;
+    public TMP_Text wordDef16;
+    public TMP_Text wordDef17;
+    public TMP_Text wordDef18;
+    public TMP_Text wordDef19;
+    public TMP_Text wordDef20;
+    public TMP_Text wordDef21;
+    public TMP_Text wordDef22;
+    public TMP_Text wordDef23;
+    public TMP_Text wordDef24;
     public TMP_Text example1;
     public TMP_Text example1Alt;
     public TMP_Text example2;
     public TMP_Text example2Alt;
     public TMP_Text jlpt;
+    public TMP_Text pageNumberText;
 
-    public Vector3 movementBetweenPagesLine1;
-    public Vector3 movementBetweenPagesLine2;
-    public Vector3 movementBetweenPagesLine3;
-    public Vector3 movementBetweenPagesLine4;
-    public Vector3 movementBetweenPagesLine5;
-    public Vector3 movementBetweenPagesLine6;
-    public Vector3 movementBetweenPagesLine7;
+    public int pageNumber = 1;
+    public GameObject backButton;
+    public GameObject line1;
+    public GameObject line2;
+    public GameObject line3;
+    public GameObject line4;
+    public GameObject line5;
+    public GameObject line6;
+
+    Vector3 movementBetweenPagesLine1;
+    Vector3 movementBetweenPagesLine2;
+    Vector3 movementBetweenPagesLine3;
+    Vector3 movementBetweenPagesLine4;
+    Vector3 movementBetweenPagesLine5;
+    Vector3 movementBetweenPagesLine6;
+    public float distanceBetweenMovement;
 
     public ENDictionary dict = new ENDictionary();
 
@@ -65,7 +89,6 @@ public class DictManager : MonoBehaviour
     {
         string query = inputField.text.ToLower();
         InfoList foundWord = dict.wordList.FirstOrDefault(word => word.word.ToLower() == query || word.kana.ToLower() == query || word.romaji.ToLower() == query);
-
         if (foundWord != null)
         {
             wordSearched.text = foundWord.word;
@@ -82,6 +105,20 @@ public class DictManager : MonoBehaviour
             wordDef8.text = foundWord.def8;
             wordDef9.text = foundWord.def9;
             wordDef10.text = foundWord.def10;
+            wordDef11.text = foundWord.def11;
+            wordDef12.text = foundWord.def12;
+            wordDef13.text = foundWord.def13;
+            wordDef14.text = foundWord.def14;
+            wordDef15.text = foundWord.def15;
+            wordDef16.text = foundWord.def16;
+            wordDef17.text = foundWord.def17;
+            wordDef18.text = foundWord.def18;
+            wordDef19.text = foundWord.def19;
+            wordDef20.text = foundWord.def20;
+            wordDef21.text = foundWord.def21;
+            wordDef22.text = foundWord.def22;
+            wordDef23.text = foundWord.def23;
+            wordDef24.text = foundWord.def24;
             example1.text = foundWord.example1;
             example1Alt.text = foundWord.example1Alt;
             example2.text = foundWord.example2;
@@ -125,6 +162,20 @@ public class DictManager : MonoBehaviour
             wordDef8.text = "";
             wordDef9.text = "";
             wordDef10.text = "";
+            wordDef11.text = "";
+            wordDef12.text = "";
+            wordDef13.text = "";
+            wordDef14.text = "";
+            wordDef15.text = "";
+            wordDef16.text = "";
+            wordDef17.text = "";
+            wordDef18.text = "";
+            wordDef19.text = "";
+            wordDef20.text = "";
+            wordDef21.text = "";
+            wordDef22.text = "";
+            wordDef23.text = "";
+            wordDef24.text = "";
             givenName.text = "";
             wordType.text = "";
             example1.text = "";
@@ -152,6 +203,20 @@ public class DictManager : MonoBehaviour
         wordDef8.text = "";
         wordDef9.text = "";
         wordDef10.text = "";
+        wordDef11.text = "";
+        wordDef12.text = "";
+        wordDef13.text = "";
+        wordDef14.text = "";
+        wordDef15.text = "";
+        wordDef16.text = "";
+        wordDef17.text = "";
+        wordDef18.text = "";
+        wordDef19.text = "";
+        wordDef20.text = "";
+        wordDef21.text = "";
+        wordDef22.text = "";
+        wordDef23.text = "";
+        wordDef24.text = "";
         givenName.text = "";
         wordType.text = "";
         example1.text = "";
@@ -173,24 +238,64 @@ public class DictManager : MonoBehaviour
             SaveToJson();
         }
     }
+    void Update()
+    {
+        pageNumberText.text = pageNumber.ToString();
+
+        if (pageNumber <= 1)
+        {
+            backButton.SetActive(false);
+        }
+        else
+        {
+            backButton.SetActive(true);
+        }
+    }
 
     public void MoveToNextSide()
     {
-        movementBetweenPagesLine1 = wordDef1.transform.position;
-        movementBetweenPagesLine1.x -= 10;
-        wordDef1.transform.position = movementBetweenPagesLine1;
+        pageNumber += 1;
+        movementBetweenPagesLine1 = line1.transform.position;
+        movementBetweenPagesLine2 = line2.transform.position;
+        movementBetweenPagesLine3 = line3.transform.position;
+        movementBetweenPagesLine4 = line4.transform.position;
+        movementBetweenPagesLine5 = line5.transform.position;
+        movementBetweenPagesLine6 = line6.transform.position;
+        movementBetweenPagesLine1.x -= distanceBetweenMovement;
+        movementBetweenPagesLine2.x -= distanceBetweenMovement;
+        movementBetweenPagesLine3.x -= distanceBetweenMovement;
+        movementBetweenPagesLine4.x -= distanceBetweenMovement;
+        movementBetweenPagesLine5.x -= distanceBetweenMovement;
+        movementBetweenPagesLine6.x -= distanceBetweenMovement;
+        line1.transform.position = movementBetweenPagesLine1;
+        line2.transform.position = movementBetweenPagesLine2;
+        line3.transform.position = movementBetweenPagesLine3;
+        line4.transform.position = movementBetweenPagesLine4;
+        line5.transform.position = movementBetweenPagesLine5;
+        line6.transform.position = movementBetweenPagesLine6;
     }
 
     public void MoveBack()
     {
-        movementBetweenPagesLine1 = wordDef1.transform.position;
-        movementBetweenPagesLine1.x += 10;
-        wordDef1.transform.position = movementBetweenPagesLine1;
-        wordDef2.transform.position = movementBetweenPagesLine1;
-        wordDef3.transform.position = movementBetweenPagesLine1;
-        wordDef4.transform.position = movementBetweenPagesLine1;
-        wordDef5.transform.position = movementBetweenPagesLine1;
-        wordDef6.transform.position = movementBetweenPagesLine1;
+        pageNumber -= 1;
+        movementBetweenPagesLine1 = line1.transform.position;
+        movementBetweenPagesLine2 = line2.transform.position;
+        movementBetweenPagesLine3 = line3.transform.position;
+        movementBetweenPagesLine4 = line4.transform.position;
+        movementBetweenPagesLine5 = line5.transform.position;
+        movementBetweenPagesLine6 = line6.transform.position;
+        movementBetweenPagesLine1.x += distanceBetweenMovement;
+        movementBetweenPagesLine2.x += distanceBetweenMovement;
+        movementBetweenPagesLine3.x += distanceBetweenMovement;
+        movementBetweenPagesLine4.x += distanceBetweenMovement;
+        movementBetweenPagesLine5.x += distanceBetweenMovement;
+        movementBetweenPagesLine6.x += distanceBetweenMovement;
+        line1.transform.position = movementBetweenPagesLine1;
+        line2.transform.position = movementBetweenPagesLine2;
+        line3.transform.position = movementBetweenPagesLine3;
+        line4.transform.position = movementBetweenPagesLine4;
+        line5.transform.position = movementBetweenPagesLine5;
+        line6.transform.position = movementBetweenPagesLine6;
     }
 }
 
@@ -221,6 +326,20 @@ public class InfoList
     public string def8;
     public string def9;
     public string def10;
+    public string def11;
+    public string def12;
+    public string def13;
+    public string def14;
+    public string def15;
+    public string def16;
+    public string def17;
+    public string def18;
+    public string def19;
+    public string def20;
+    public string def21;
+    public string def22;
+    public string def23;
+    public string def24;
     public string example1;
     public string example1Alt;
     public string example2;

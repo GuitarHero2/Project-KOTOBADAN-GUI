74b8e83d4db1eec47f4884f34971868d0f2b3a0c
Kobayashi Nozomi <159567987+GuitarHero2@users.noreply.github.com>
2024-09-08 18:09:46 +0200

    Micro update
    In process of adding the info button for the Dictionaries.

---
+++ Assets/Scripts/MainMenu/Screen Settings.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenSettings : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void ChangeResolution(int height, int width)
+    {
+
+    }
+}

2dd141a319a1e78f78c0cee7a30e2a7da4738a0c
Nozomi2225 <159567987+GuitarHero2@users.noreply.github.com>
2024-09-10 12:10:42 +0200

    Resolution Update
    -Added Resolution options for 16:9

--- Assets/Scripts/MainMenu/Screen Settings.cs
+++ Assets/Scripts/MainMenu/Screen Settings.cs
@@ -1,23 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScreenSettings : MonoBehaviour
 {
-
-    void Start()
-    {
-        
-    }
-
+    //DEBUG
+    public TMP_Text currentWidth;
+    public TMP_Text currentHeight;
 
     void Update()
     {
-        
+        currentHeight.text = Screen.height.ToString();
+        currentWidth.text = Screen.width.ToString();
     }
 
-    public void ChangeResolution(int height, int width)
+    public void SetResolutionTo4K()
     {
-
+        Screen.SetResolution(3840, 2160, Screen.fullScreen == false);
+    }
+    public void SetResolutionToQHD()
+    {
+        Screen.SetResolution(2560, 1440, Screen.fullScreen == false);
+    }
+    public void SetResolutionToFHD()
+    {
+        Screen.SetResolution(1920, 1080, Screen.fullScreen == false);
+    }
+    public void SetResolutionToHDPlus()
+    {
+        Screen.SetResolution(1600, 900, Screen.fullScreen == false);
+    }
+    public void SetResolutionToHDTV()
+    {
+        Screen.SetResolution(1366, 768, Screen.fullScreen == false);
+    }
+    public void SetResolutionToHD()
+    {
+        Screen.SetResolution(1280, 720, Screen.fullScreen == false);
     }
 }
